b647773f5a209dca4ad2a9b280565b3fc2c91702
Jiwon <lunaseaj@naver.com>
2026-03-15 16:16:48 +0000

    github page 배포

---
+++ src/routes/+layout.ts
@@ -0,0 +1,3 @@
+// src/routes/+layout.ts
+export const prerender = true;
+export const ssr = false;
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,10 +12,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html', // SPA 라우팅을 위해 필요
+			precompress: false,
+			strict: true
+		}),
 		alias: {
 			"@/*": "./path/to/lib/*",
 		},
+		paths: {
+   		   base: process.argv.includes('dev') ? '' : '/svelte-konva'
+   		}
 	}
 };
 
